7e231063820e2927d51c4497078c8b1c43eac6bf
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-02-01 06:13:20 +0530

    Completed the Features and VideoPreview Part

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,26 @@
-const Button = () => {
+import clsx from "clsx";
+
+const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
   return (
-    <div>
-      
-    </div>
+    <button 
+        id={id}
+        className={clsx(
+            "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black",
+            containerClass
+        )}
+    >
+        {leftIcon}
+
+        <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+            <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
+                {title}
+            </div>
+            <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
+                {title}
+            </div>
+        </span>
+    </button>
   )
 }
 
-export default Button
+export default Button;
